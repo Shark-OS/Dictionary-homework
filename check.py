5dcf65362adb8cfe1ec489fd6184dc789a7b5dd3
Shark-OS <sharwin.ekbote@gmail.com>
2024-10-05 11:10:08 -0500

    Add files via upload

---
+++ check.py
@@ -0,0 +1,12 @@
+capital= {'India':'New Delhi','USA':'Washington DC','France':'Paris'}
+print(capital)
+print(capital['France'])
+print(capital.keys())
+print(capital.values())
+print(len(capital))
+capital['Australia']='Sydney'
+print(capital)
+
+student_grades={'Ashton': 73, 'Brent': 77, 'Cary': 80, 'Drake': 82, 'Ella': 83, 'Finn': 86, 'Grace': 89, 'Henry': 91, 'Iris': 95, 'Jake': 98, 'Kate': 100}
+print(student_grades)
+print(student_grades.values)
